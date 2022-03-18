13804ee1955f3898da566a65c675d663ea755447
DylanCheetah <dylan_the_cheetah@outlook.com>
2022-03-17 22:32:59 -0400

    Finished weather importer.

--- WeatherImporter/scripts/Main.gd
+++ WeatherImporter/scripts/Main.gd
@@ -45,6 +45,8 @@ func _on_WeatherFolderSelectScreen_next(old_dir, new_dir):
 	else:
 		print("Failed to load custom weather cycles.")
 		
+	file.close()
+		
 	#Remove trash from weather cycles array
 	var i = 0
 	
@@ -64,53 +66,9 @@ func _on_WeatherFolderSelectScreen_next(old_dir, new_dir):
 	var data = [old_dir, new_dir, cycles]
 	var thread = Thread.new()
 	thread.start(self, "import_weather_thread", data)
+	#import_weather_thread(data)
 	
 	
-func import_weather_thread(data):
-	var old_dir = data[0]
-	var new_dir = data[1]
-	var cycles = data[2]
-	
-	#Import weather config file
-	emit_signal("begin_step", tr("IMPORTING_WEATHER"))
-	var file = File.new()
-	var weather = {}
-	
-	if not file.open(old_dir + "/Weather.cfg", File.READ):
-		pass #import weather here
-		
-	emit_signal("end_step")
-	
-	#Import custom weather config file
-	emit_signal("begin_step", tr("IMPORTING_CUSTOM_WEATHER"))
-	
-	if not file.open(old_dir + "/CustomWeather.cfg", File.READ):
-		pass #import custom weather here
-		
-	emit_signal("end_step")
-	
-	#Import weather cycles here
-	for cycle in cycles:
-		cycle = cycle.split("\n")
-		cycle[0] = cycle[0].replace("]", "")
-		emit_signal("begin_step", 
-		    tr("IMPORTING_WEATHER_CYCLE") + " '" + cycle[0] +
-		    "'...")
-		
-		#import weather cycle here
-		OS.delay_msec(1000)
-		
-		emit_signal("end_step")
-	
-	#Emit import complete signal
-	emit_signal("import_complete")
-		
-		
-func show_error(msg):
-	get_node("ErrorDialog").set_text(msg)
-	get_node("ErrorDialog").popup()
-
-
 func _on_Main_begin_step(msg):
 	get_node("ImportProgressScreen").begin_step(msg)
 
@@ -123,3 +81,181 @@ func _on_Main_import_complete():
 	#Switch back to weather folder select screen
 	get_node("ImportProgressScreen").hide()
 	get_node("WeatherFolderSelectScreen").show()
+	
+	
+func import_weather_thread(data):
+	var old_dir = data[0]
+	var new_dir = data[1]
+	var cycles = data[2]
+	
+	#Import weather config file
+	emit_signal("begin_step", tr("IMPORTING_WEATHER"))
+	var weather = {
+	    "weather": {},
+	    "cycles": {}
+	}
+	import_weather(old_dir + "/Weathers.cfg", weather)
+	emit_signal("end_step")
+	
+	#Import custom weather config file
+	emit_signal("begin_step", tr("IMPORTING_CUSTOM_WEATHER"))
+	import_weather(old_dir + "/CustomWeathers.cfg", weather)
+	emit_signal("end_step")
+	
+	#Import weather cycles here
+	for cycle in cycles:
+		cycle = cycle.split("\n")
+		cycle[0] = cycle[0].replace("]", "")
+		emit_signal("begin_step", 
+		    tr("IMPORTING_WEATHER_CYCLE") + " '" + cycle[0] +
+		    "'...")
+		import_weather_cycle(cycle[0], 
+		    old_dir + "/" + cycle[1], weather)
+		emit_signal("end_step")
+		
+	#Save weather file
+	var file = File.new()
+	
+	if file.open(new_dir + "/weather.json", File.WRITE):
+		print(tr("FAILED_TO_SAVE") + " '" + new_dir + 
+		    "/weather.json'.")
+		return
+		
+	file.store_string(pretty_json(weather.to_json()))
+	file.close()
+	
+	#Emit import complete signal
+	emit_signal("import_complete")
+	
+	
+func import_weather(filename, weather):
+	#Load weather data
+	var file = File.new()
+	
+	if file.open(filename, File.READ):
+		print(tr("FAILED_TO_IMPORT_WEATHER") + " '" + 
+		    filename + "'.")
+		return
+		
+	var sections = file.get_as_text().split("[")
+	file.close()
+	
+	#Parse weather data
+	for section in sections:
+		#Skip empty sections
+		if section == "":
+			continue
+		
+		#Split section into lines
+		section = section.replace("\r\n", "\n").split("\n")
+		section[0] = section[0].replace("]", "")
+		
+		#Import weather data
+		var name = section[0]
+		var particle = section[1].split("/")[1]
+		var pos = section[2].split_floats(" ")
+		var sound = section[3] if section.size() >= 4 else ""
+		weather["weather"][name] = {
+		    "particle": particle,
+		    "pos": pos,
+		    "sound": sound
+		}
+	
+	
+func import_weather_cycle(name, filename, weather):
+	#Load weather cycle data
+	var file = File.new()
+	
+	if file.open(filename, File.READ):
+		print(tr("FAILED_TO_IMPORT_WEATHER_CYCLE") + " '" +
+		    filename + "'.")
+		return
+		
+	var sections = file.get_as_text().split("[")
+	file.close()
+	
+	#Parse weather cycle data
+	var cycle = []
+	
+	for section in sections:
+		#Skip empty sections
+		if section == "":
+			continue
+			
+		#Split section into lines
+		section = section.replace("\r\n", "\n").split("\n")
+		section[0] = section[0].replace("]", "")
+		
+		#Import weather cycle data
+		var phase = {
+		    "sky": {}
+		}
+		
+		for item in section:
+			#Separate key and value
+			item = item.split("=")
+			
+			#Start time?
+			if item[0] == "Start":
+				phase["start"] = int(item[1])
+				
+			#End time?
+			elif item[0] == "End":
+				phase["end"] = int(item[1])
+				
+			#Sky shader?
+			elif item[0] == "SkyShader":
+				phase["sky"]["shader"] = item[1].split_floats(" ")
+				
+			#Sky adder?
+			elif item[0] == "SkyAdder":
+				phase["sky"]["adder"] = item[1].split_floats(" ")
+				
+			#Weather?
+			elif item[0] == "Weather":
+				phase["weather"] = item[1]
+				
+			#Rate?
+			elif item[0] == "Rate":
+				phase["rate"] = int(item[1])
+				
+		cycle.append(phase)
+		
+	weather["cycles"][name] = cycle
+		
+		
+func show_error(msg):
+	get_node("ErrorDialog").set_text(msg)
+	get_node("ErrorDialog").popup()
+	
+	
+func pretty_json(json):
+	var pretty_json = ""
+	var indent = ""
+	
+	for char in json.replace(", ", ","):
+		#Add newline after opening curly braces and square 
+		#brackets. Also, increase indentation level by one.
+		if char in ["{", "["]:
+			indent += "    "
+			pretty_json += char + "\r\n" + indent
+			
+		#Decrease indentation level before closing curly braces
+		#and square brackets. Also, add newline before them.
+		elif char in ["}", "]"]:
+			indent.erase(0, 4)
+			pretty_json += "\r\n" + indent + char
+			
+		#Add newline after commas
+		elif char == ",":
+			pretty_json += char + "\r\n" + indent
+			
+		#Retain indentation level
+		else:
+			if char == "\n":
+			    pretty_json += "\r\n" + indent
+			
+			else:
+				pretty_json += char
+		
+	return pretty_json
