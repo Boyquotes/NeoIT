eb03f055bc000a8da1201fe48ade6ed7d363be25
DylanCheetah <dylan_the_cheetah@outlook.com>
2022-03-21 01:16:29 -0400

    Added clouds, sun, and moon.

--- Game/scripts/SkyManager.gd
+++ Game/scripts/SkyManager.gd
@@ -1,5 +1,8 @@
 extends Spatial
 
+export (ShaderMaterial) var sky_mat
+export (ShaderMaterial) var cloud_mat
+
 var weather = {}
 var user_weather = {}
 
@@ -27,12 +30,26 @@ func _ready():
 	else:
 		logger.log_warning("Failed to load user weather.")
 		
+	#Set materials
+	get_node("SkySphere/sky_sphere").set_material_override(sky_mat)
+	get_node("CloudSphere/cloud_sphere").set_material_override(cloud_mat)
+	
+	#Turn on billboard mode for the sun
+	get_node("CelestialPivot/SunSprite/sun").set("geometry/billboard", true)
+	get_node("CelestialPivot/MoonSprite/moon").set("geometry/billboard", true)
+	
 	#Enable event processing
 	set_process(true)
 	
 	
 func _process(delta):
-	#Move the sky dome with the camera
+	#Move the sky and cloud domes with the camera
 	var transform = get_node("SkySphere").get_transform()
 	transform.origin = get_viewport().get_camera().get_transform().origin
 	get_node("SkySphere").set_transform(transform)
+	get_node("CloudSphere").set_transform(transform)
+	
+	#Move the celestial pivot too
+	transform = get_node("CelestialPivot").get_transform()
+	transform.origin = get_viewport().get_camera().get_transform().origin
+	get_node("CelestialPivot").set_transform(transform)
